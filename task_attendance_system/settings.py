ef5fccc789729043d016accd91204a7cf67fab2d
Subhashree <143589528+SubhashreeMahapatra@users.noreply.github.com>
2026-05-30 11:13:03 +0530

    Change database engine from MySQL to PostgreSQL

--- task_attendance_system/settings.py
+++ task_attendance_system/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'task_attendance_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.pstgresql',
         'NAME': config('DB_NAME', default='task_attendance_db'),
         'USER': config('DB_USER', default='root'),
         'PASSWORD': config('DB_PASSWORD', default=''),
